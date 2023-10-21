d0223d9f3154bdc8e88d35ef42b8449c53574466
acbst0 <125031376+acbst0@users.noreply.github.com>
2023-10-21 14:42:26 +0300

    21.10.2023 | 2:43

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abostano <abostano@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 12:50:33 by abostano          #+#    #+#             */
+/*   Updated: 2023/10/21 13:09:20 by abostano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	if (lst && *lst)
+	{
+		ft_lstcşear(&(*lst)->next, del);
+		ft_lstdelone(*lst, del);
+		*lst = 0;
+	}
+}
